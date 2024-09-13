a5e2e14b00fee9e84bbdf35ca428a0c73ca5e29f
Pamusah <musahpaulafriyie@gmail.com>
2024-09-13 14:24:46 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Weather from './components.jsx/Weather'
+const App = () => {
+  return (
+    <div className="app"><Weather/>
+    </div>
+  )
+}
+
+export default App
